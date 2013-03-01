45e24ce51a9669c1036de9cc0438ae9da0c3c87c
Xiangyu Shen <phil@Xiangyus-MacBook-Air.local>
2013-03-01 14:41:23 -0600

    ADD:first commit.

---
+++ pgm/Factor.java
@@ -0,0 +1,9 @@
+package pgm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factor {
+     List<Variable> variables = new ArrayList<Variable>();
+     List<Double> table = new ArrayList<Double>(); 
+}
---
+++ pgm/Variable.java
@@ -0,0 +1,7 @@
+package pgm;
+
+public class Variable {
+     private int d;
+     private boolean is_evidence;
+     private int value;
+}
