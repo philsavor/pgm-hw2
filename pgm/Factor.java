dfc6c11416703f78f5537da3de9c7f2cb05ac0e4
Xiangyu Shen <phil@Xiangyus-MacBook-Air.local>
2013-03-04 11:40:15 -0600

    before due version.

--- pgm/Factor.java
+++ pgm/Factor.java
@@ -4,6 +4,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Factor {
-     List<Variable> variables = new ArrayList<Variable>();
-     List<Double> table = new ArrayList<Double>(); 
+     private List<Variable> variables = new ArrayList<Variable>();
+     private List<Double> table = new ArrayList<Double>(); 
+     
+     public Factor(List<Variable> vars , List<Double> tab){
+    	 this.variables = vars;
+    	 this.table = tab;
+     }
+     
+     public Factor(){
+    	 this.variables = new ArrayList<Variable>();
+    	 this.table = new ArrayList<Double>();
+     }
+     
+     //add a variable
+     public void addVariable(Variable v){
+    	 this.variables.add(v);
+     }
+     
+     //get variable
+     public Variable getVariable(int index){
+    	 return this.variables.get(index);
+     }
+     
+     //get all variables
+     public List<Variable> getAllVariables(){
+    	 return this.variables;
+     }
+     
+     //get # variables
+     public int getNumOfVariables(){
+    	 return this.variables.size();
+     }
+     
+     //add value to table
+     public void addValueToTable(Double value){
+    	 this.table.add(value);
+     }
+     
+     //get # values in table
+     public int getNumOfValuesInTable(){
+    	 return this.table.size();
+     }
+     
+     //get value from table
+     public Double getValueFromTable(int index){
+    	 return this.table.get(index);
+     }
+     
+     //remove value in table : instantiate
+     public void removeValueInTable(int index){
+    	 this.table.remove(index);
+     }
+     
+     //get all values in table
+     public List<Double> getAllValuesInTables(){
+    	 return this.table;
+     }
+     
+     //for test
+     public  void showTableValues(){
+    	 System.out.println(this.table);
+     }
 }
--- pgm/Variable.java
+++ pgm/Variable.java
@@ -1,7 +1,48 @@
 package pgm;
 
 public class Variable {
-     private int d;
-     private boolean is_evidence;
-     private int value;
+     private int size;  //domain size
+     private boolean isEvidence;
+     private int evidValue;
+     private int index;  // the index of variable
+     
+     public Variable(){
+    	 this.isEvidence = false;
+     }
+     
+     //evidence value
+     public void setEvidValue(int value){
+    	 this.evidValue = value;
+     }
+     
+     public int getEvidValue(){
+    	 return this.evidValue;
+     }
+     
+     //size
+     public void setSize(int size){
+    	 this.size = size;
+     }
+     
+     public int getSize(){
+    	 return this.size;
+     }
+     
+     //isEvidence
+     public void setEvidence(){
+    	 this.isEvidence = true;
+     }
+     
+     public boolean getEvidence(){
+    	 return this.isEvidence;
+     }
+     
+     //index
+     public void setIndex(int index){
+    	 this.index = index;
+     }
+     
+     public int getIndex(){
+    	 return this.index;
+     }
 }
